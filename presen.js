1666c83d07594b8acf20c051c963847cac1ae659
SHIMOMURA Sho <shimo@agusa.i.is.nagoya-u.ac.jp>
2009-09-11 15:30:05 +0900

    hoge

--- presen.js
+++ presen.js
@@ -1,14 +1,55 @@
-function init(){
+// TODO: プレゼンの中身を考慮した遷移をできるように
+function PresenPager(lastPage, lastThumbsPage, numOfThumbs){ 
+   this.adjustThumbsPage = function(){
+      this.currentThumbsPage =
+      Math.floor(this.currentPage / numOfThumbs);
+   };
+
+   this.lastPage = lastPage;
+   this.lastThumbsPage = lastThumbsPage;
+   this.currentPage = 0;
+   this.currentThumbsPage = 0;
+   return this;
+}
+PresenPager.prototype.nextPage = function(){
+   if (this.currentPage < this.lastPage-1) {
+      this.currentPage++;
+      this.adjustThumbsPage();
+   }
+};
+PresenPager.prototype.prevPage = function(){
+   if (this.currentPage > 0) {
+      this.currentPage--;
+      this.adjustThumbsPage();
+   }
+};
+PresenPager.prototype.setPage = function(idx){
+   this.currentPage = idx;
+   this.adjustThumbsPage();
+};
+PresenPager.prototype.nextThumbsPage = function(){
+   if (this.currentThumbsPage < this.lastThumbsPage) {
+      this.currentThumbsPage++;
+   }
+};
+PresenPager.prototype.prevThumbsPage = function(){
+   if (this.currentThumbsPage > 0) {
+      this.currentThumbsPage--;
+   }
+};
+
+function initializePresen(pageWidth, pageHeight, numOfThumbsInRow){
    const Config = {
-         'pageWidth':800, 'pageHeight':600,
+         'pageWidth': pageWidth,
+         'pageHeight': pageHeight,
+         'numOfThumbsInRow': numOfThumbsInRow,
+         'numOfPagesInThumbsPage': Math.pow(numOfThumbsInRow, 2),
          'intervalOfPages': 20,
-         'numOfThumbsInRow': 3,
-         'numOfPagesInThumbsPage':9
    }
    const keymap = {'left':37, 'up':38, 'right':39, 'down':40};
    var pagedata;
-   var currentPage = 0;
-   var currentThumbsPage = 0;
+
+
 
    const viewMode   = 0;
    const thumbsMode = 1;
@@ -23,8 +64,8 @@ function init(){
          var scale;
          switch (currentMode) {
          case viewMode:
-            x = pageWidth * (i-currentPage);
-            y = pagedata[i].posY;
+            x = pageWidth * (i-pager.currentPage);
+            y = 0;
             scale = 1;
             
             break;
@@ -35,7 +76,7 @@ function init(){
 
             x = defaultX +
                (Math.floor(i/Config.numOfPagesInThumbsPage) -
-                currentThumbsPage) *
+                pager.currentThumbsPage) *
                (thumbsPageWidth+pageWidth/2);
             y = defaultY;
             
@@ -54,12 +95,17 @@ function init(){
       pages[i].style.zIndex = 1000-i;
       pages[i].style.top  = (window.innerHeight-Config.pageHeight)/2;
       pages[i].style.left = (window.innerWidth-Config.pageWidth)/2;
+      pages[i].addEventListener('click', (function(i){
+         return function(e){
+            pager.setPage(i);
+            currentMode = viewMode;
+            translatePage();
+         }
+      })(i), false);
       
       // calculate standard positions for all pages
       var pagedatum = {
          page: pages[i],
-         posX: (Config.pageWidth + Config.intervalOfPages) * i,
-         posY: 0,
          thumbPosX: 
             (Config.pageWidth + Config.intervalOfPages) *
             (i%Config.numOfThumbsInRow - 1),
@@ -69,38 +115,33 @@ function init(){
                         Config.numOfThumbsInRow) - 1)
       };
       pages[i].style.webkitTransform = 
-         "scale(1) translate("+pagedatum.posX+"px, "+pagedatum.posY+"px)";
-
+         "scale(1) translate("+
+         ((Config.pageWidth + Config.intervalOfPages) * i)+"px, 0px)";
 
       pagedata[i] = pagedatum;
    }
+
+   var pager = new PresenPager(pagedata.length,
+                               Math.floor(pagedata.length /
+                                          Config.numOfPagesInThumbsPage),
+                               Config.numOfPagesInThumbsPage);
 
    document.body.addEventListener("keyup", function(e){
       switch(e.keyCode) {
       case keymap.left:
          if (currentMode == viewMode) {
-            if (currentPage > 0) {
-               currentPage -= 1;
-            }
+            pager.prevPage();
          } else if (currentMode == thumbsMode) {
-            if (currentThumbsPage > 0) {
-               currentThumbsPage -= 1;
-            }
+            pager.prevThumbsPage();
          }
          translatePage();
          break;
 
       case keymap.right:
          if (currentMode == viewMode) {
-            if (currentPage < pagedata.length-1) {
-               currentPage += 1;
-            }
+            pager.nextPage();
          } else if (currentMode == thumbsMode) {
-            if (currentThumbsPage <
-                Math.floor(pagedata.length/
-                           Config.numOfPagesInThumbsPage)) {
-                              currentThumbsPage += 1;
-                           }
+            pager.nextThumbsPage();
          }
          translatePage();
          break;
